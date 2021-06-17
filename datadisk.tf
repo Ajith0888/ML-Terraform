6c87c78ce5759bb58bb676682d862116c39210a8
Ajith Kumar M <ajithkumar.mohanakumaran@marklogic.com>
2021-06-17 08:51:17 +0200

    Updated VM Build

---
+++ datadisk.tf
@@ -0,0 +1,23 @@
+  resource "azurerm_managed_disk" "mldisk" {
+  name                 = "${var.vmname}-disk1"
+  location             = azurerm_resource_group.rg-ajith-oneamerica.location
+  resource_group_name  = azurerm_resource_group.rg-ajith-oneamerica.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 32
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "mldisk" {
+  managed_disk_id    = azurerm_managed_disk.mldisk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.mlvm.id
+  lun                = "0"
+  caching            = "ReadOnly"
+
+  depends_on = [
+   azurerm_linux_virtual_machine.mlvm
+   ]
+
+
+}
+
+
